2f93e4db1ca83779074ae3854b52647232f98625
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-03-26 23:20:17 +0600

    header added

--- src/conponents/header/Header.jsx
+++ src/conponents/header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
     return (
-        <div>
-            <h3>Header</h3> 
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="logo" />
+
+            <div>
+                <a href="/Order">Order</a>
+                <a href="/Order Review">Order Review</a>
+                <a href="/ManageInventory">Manage Inventory</a>
+                <a href="/Login">Login</a>
+            </div>
+        </nav>
     );
 };
 
